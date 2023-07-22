e1c983699edd6b09a9cacbc5eeea1733060bd77d
Tim Jarratt <tjarratt@gmail.com>
2023-07-23 00:23:20 +0200

    Make directory for nvim before copying in config

--- install.sh
+++ install.sh
@@ -19,10 +19,15 @@ asdf global elixir 1.15.0-otp-26
 sed -i '' 's/plugins=.*/plugins=(git z)/' ~/.zshrc
 grep -qxF '. /usr/local/opt/asdf/libexec/asdf.sh' ~/.zshrc || echo '. /usr/local/opt/asdf/libexec/asdf.sh' >> ~/.zshrc
 
+# nvim setup
+mkdir -p ~/.config/nvim
 ln -sf ~/workspace/workstation/init.lua ~/.config/nvim/init.lua
-git config --global --add url."git@github.com:".insteadOf "https://github.com/"
 nvim -es ./update-plugins.txt
 
+# git setup
+git config --global --add url."git@github.com:".insteadOf "https://github.com/"
+
+# dotfiles
 cp dotfiles/.vimrc $HOME/.vimrc
 cp dotfiles/.zshrc $HOME/.zshrc
 cp dotfiles/.iex.exs $HOME/.iex.exs
